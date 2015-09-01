25a952fe5ad6e95b5d922833ad1ca3a0e5576a77
lockewood <akull3@gatech.edu>
2015-09-01 10:48:48 -0400

    Added initial code

--- HW2.py
+++ HW2.py
@@ -4,3 +4,24 @@
 # akull3@gatech.edu 903135072
 # We worked on the homework assignment alone, 
 # using only this semester's course materials.
+
+def square(base):
+    result = base ** 2
+    return result
+
+def taylorSwift(numFans):
+    time = numFans * 3
+    mins = time % 60
+    h = time // 60
+    hrs = 9 + h
+    if hrs > 12:
+        hrs = hrs - 12
+        print("Taylor Swift is done at " + str(hrs) + ":" + str(mins) + " AM.")
+    elif hrs < 12:
+        print("Taylor Swift is done at " + str(hrs) + ":" + str(mins) + " PM.")
+    else:
+        print("Taylor Swift is done at " + str(hrs) + ":" + str(mins) + " AM.")
+
+def girlScoutCookies():
+    b = int( input("How Many Boxes Do You Want?") )
+    m = float( input("How Much Money Do You Have?") )
