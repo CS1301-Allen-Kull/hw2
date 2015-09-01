1cb4424c6b5ff485712b05fced47d331a133c664
Tristen Allen <spyboticsguy@gmail.com>
2015-09-01 10:36:15 -0400

    initial commit

---
+++ HW2.py
@@ -0,0 +1,6 @@
+# Tristen Allen - Section A4
+# tristen.allen@gatech.edu 903035156
+# Adam Kull - Section A4
+# akull3@gatech.edu 903135072
+# We worked on the homework assignment alone, 
+# using only this semester's course materials.
